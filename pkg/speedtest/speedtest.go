b6624ef101472360c12320c9d280c63e797e8762
Robson Cruz <ghost0092@gmail.com>
2025-05-01 15:54:08 -0300

    feat(pks/speedtest): add SpeedtestGoProvider base

--- pkg/speedtest/speedtest.go
+++ pkg/speedtest/speedtest.go
@@ -2,7 +2,10 @@ package speedtest
 
 import (
 	"fmt"
+	"log"
 	"time"
+
+	"github.com/showwin/speedtest-go/speedtest"
 )
 
 // Result holds the result of a speedtest
@@ -22,4 +25,24 @@ func RunSpeedtest() (Result, error) {
 		PingMs:    15.0,
 		Timestamp: time.Now(),
 	}, nil
+}
+
+// TODO: Add other providers for cases we want to use the CLI directly
+// or some tool that is able to interface with ours given properly parsable output
+// The idea of providers is that we would be able to further extend the supported speed test solutiosn we have
+
+// SpeedtestGoProvider is a speed test provider that uses the showwin/speedtest-go library
+type SpeedtestGoProvider struct {
+	user *speedtest.User
+}
+
+// NewSpeedtestGoProvider creats a new SpeedtestGoProvider
+func NewSpeedtestGoProvider() (*SpeedtestGoProvider, error) {
+	user, err := speedtest.FetchUserInfo()
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch speedtest user info: %w", err)
+	}
+
+	log.Printf("Speedtest user info: %+v\n", user)
+	return &SpeedtestGoProvider{user: user}, nil
 }
